cb8092b57f6262045ff631535fa4d6a38bffd341
Chuck Ries <Chuck.Ries@microsoft.com>
2016-10-11 10:14:39 -0700

    initial commit

---
+++ My Console Application/My Folder/My Class.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Console_Application.My_Folder
+{
+    class My_Class
+    {
+        public static void Method()
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
--- My Console Application/My Program.cs
+++ My Console Application/My Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using My_Console_Application.My_Folder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace My_Console_Application
     {
         static void Main(string[] args)
         {
+            My_Class.Method();
         }
     }
 }
